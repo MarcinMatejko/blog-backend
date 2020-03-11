8324a4bdfb78e7bfab0127f73d218518ce195510
Marcin Matejko <matejko.tsp@gmail.com>
2020-03-11 17:02:03 +0100

    przygotowanie do deploymentu

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const path = require('path');
 
 dotenv.config({ path: './config/config.env'});
 
@@ -13,6 +14,16 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1/posts', posts);
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build')); 
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
